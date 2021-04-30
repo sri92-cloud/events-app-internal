9e878a7847768349da91731fed0aee09771f99f2
sri92-cloud <srivani.bandaru27@gmail.com>
2021-04-30 20:59:34 +0000

    added firestone code to server.js

--- server.js
+++ server.js
@@ -23,9 +23,6 @@ const mockEvents = {
     ]
 };
 
-
-
-
 // health endpoint - returns an empty array
 app.get('/', (req, res) => {
     res.json([]);
@@ -73,3 +70,52 @@ const server = app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+// bring in firestore
+const Firestore = require("@google-cloud/firestore");
+
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+
+app.post('/event', (req, res) => {
+    // create a new object from the json data and add an id
+    const ev = { 
+        title: req.body.title, 
+        description: req.body.description,
+        id : mockEvents.events.length + 1
+     }
+// this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
+});
+
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                 res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
+
+app.get('/events', (req, res) => {
+    getEvents(req, res);
+});
+
